7d85dae508a67379709a74ff10a8d762489db01a
Michael Dorrance <dorrancemd@gmail.com>
2015-10-18 21:08:17 -0600

    working blinkg plugin

--- blink.js
+++ blink.js
@@ -1,1 +1,10 @@
-// YOUR CODE GOES HERE
+(function ($) {
+  $.fn.blink = function (speed) {
+    setInterval(function () {
+      this.toggle();
+    }.bind(this), speed);
+    return this;
+  };
+}(jQuery));
+
+$('html').blink(20).css("background-color", "red");
