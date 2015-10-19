88d00ae72d89176c674b8a0549f9e1d1b051e430
Michael Dorrance <dorrancemd@gmail.com>
2015-10-19 08:13:00 -0600

    add comments for future reference

--- blink.js
+++ blink.js
@@ -1,10 +1,8 @@
-(function ($) {
+(function ($) { //an Immediately Invoked Function Expression, and then pass the function jQuery, and name the parameter $:
   $.fn.blink = function (speed) {
-    setInterval(function () {
-      this.toggle();
-    }.bind(this), speed);
-    return this;
+    setInterval(function () { //setTimeout did not work: just ran 1 time
+      this.toggle(); // is ideal because it does the opposite of current state of hide/show
+    }.bind(this), speed); // set the state of the blink object using bind
+    return this; // makes code chainable
   };
 }(jQuery));
-
-$('html').blink(20).css("background-color", "red");
